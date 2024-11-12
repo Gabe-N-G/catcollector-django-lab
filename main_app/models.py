2c7e36dfa4a44c5928d5644b977b8ccd7192a43a
g g <gabedotgutierrez@gmail.com>
2024-11-12 15:13:58 -0500

    feeding mostly done

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,12 @@ from django.db import models
 # Create your models here.
 # imagine models/schema
 
+# Allows the 3 letter options of Feeding.meal
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 # Serializers part 4.
 class Cat(models.Model):
     name = models.CharField(max_length=100)
@@ -15,3 +21,24 @@ class Cat(models.Model):
     def __str__(self):
         return self.name
     
+class Feeding(models.Model):
+    date = models.DateField('Feeding Date')
+    meal = models.CharField(max_length=1,
+    # add the 'choices' field option
+    choices=MEALS,
+    # set the default value for meal to be 'B'
+    default=MEALS[0][0]
+    )
+
+    # Create a cat_id FK
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+    # Nice method for obtaining the friendly value of a Field.choice
+    # get_x_display() is a built in python method. It generates the actual name of the thing.
+    # This is only if you have choices that are different displayed than whats stored in database.
+        return f"{self.get_meal_display()} on {self.date}"
+    
+    # changing ordering (latest date (closest to present) on top)
+    class Meta:
+        ordering = ['-date']
--- main_app/serializers.py
+++ main_app/serializers.py
@@ -1,5 +1,7 @@
+# Serializers are used for a restful API so we can communicate with backend to front end from json/sql to html and back.
+
 from rest_framework import serializers
-from .models import Cat
+from .models import Cat, Feeding
 
 class CatSerializer(serializers.ModelSerializer):
     # Meta is a pseudocalss to add the Model with the fields.
@@ -7,3 +9,9 @@ class CatSerializer(serializers.ModelSerializer):
         model = Cat
         # Can also select specific fields.
         fields = '__all__'
+
+class FeedingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Feeding
+        fields = '__all__'
+        read_only_fields = ('cat',)
